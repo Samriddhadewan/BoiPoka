d07e3ca82eef704a7243dd6d5f7d92758216e87b
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-01-21 18:25:32 +0600

    the book details section is on progress

--- src/Components/BookDetails/BookDetails.jsx
+++ src/Components/BookDetails/BookDetails.jsx
@@ -7,10 +7,20 @@ const BookDetails = () => {
 
     const book = books.find(book => book.bookId === id)
 
-    const {image} = book;
+    const {image, bookName, author, category, review, rating, tags} = book;
   return (
-    <div>
-        <h2>{bookId}</h2>
+    <div className="my-9 flex gap-10 justify-center">
+        <div className=" bg-[#F3F3F3] rounded-2xl p-20">
+        <img className="w-80" src={image} alt="" />
+        </div>
+        <div className="flex-1">
+        <h1 className="text-7xl text-[#131313] font-bold">{bookName}</h1>
+        <p className="font-normal my-3 text-xl text-[#131313]">by :{author}</p>
+        <hr />
+            <p className="font-normal  text-base text-[#131313] my-4">{category}</p>
+        <hr />
+        <p className="text-[#131313CC] my-3 text-sm"><b className="text-lg">Review</b> : {review} </p>
+        </div>
     </div>
   )
 }
